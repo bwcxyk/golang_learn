be481d33044271deccf76e2f6038bc2b8ae198af
不忘初心 <1624717079@qq.com>
2023-08-03 14:10:57 +0800

    2023-8-3

---
+++ 2023/oracle/oracle.go
@@ -0,0 +1,66 @@
+/*
+@Author : YaoKun
+@Time : 2023/8/3 13:42
+*/
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"sync"
+	"time"
+
+	go_ora "github.com/sijms/go-ora/v2"
+)
+
+func Query(ctx context.Context, db *sql.DB, query string, wg *sync.WaitGroup) error {
+	defer wg.Done()
+
+	// 使用context执行查询
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	// 处理查询结果
+	for rows.Next() {
+		var result string
+		err := rows.Scan(&result)
+		if err != nil {
+			fmt.Println("读取查询结果时出错:", err)
+			continue
+		}
+		fmt.Println("查询结果:", result)
+	}
+
+	return nil
+}
+
+func main() {
+	dsn := go_ora.BuildUrl("192.168.1.70", 1521, "orcl", "tms_user", "123456", nil)
+
+	// 连接数据库
+	db, err := sql.Open("oracle", dsn)
+	if err != nil {
+		fmt.Println("连接数据库时出错:", err)
+		return
+	}
+	defer db.Close() // 当主函数执行完毕时关闭数据库连接
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// 使用context设置查询超时时间
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel() // 当主函数执行完毕时确保context被取消
+
+	// 使用context运行查询
+	err = Query(ctx, db, "select sysdate from dual", &wg)
+	if err != nil {
+		fmt.Println("执行查询时出错:", err)
+	}
+
+	wg.Wait()
+}
