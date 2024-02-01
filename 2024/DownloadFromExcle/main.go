fdffba579a39b6ac9677002c76484c5c74e7501d
不忘初心 <1624717079@qq.com>
2024-02-01 15:32:58 +0800

    优化

--- 2024/DownloadFromExcle/main.go
+++ 2024/DownloadFromExcle/main.go
@@ -1,109 +1,132 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"path"
-	"sync"
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+    "path"
+    "sync"
 
-	"github.com/gen2brain/dlgs"
-	"github.com/xuri/excelize/v2"
+    "github.com/gen2brain/dlgs"
+    "github.com/xuri/excelize/v2"
 )
 
 // 下载函数，从指定URL下载文件并保存为指定文件名
 func download(fileName, url string, wg *sync.WaitGroup, sem chan struct{}) {
-	defer wg.Done()
-	sem <- struct{}{} // 获取信号量
+    defer wg.Done()
+    sem <- struct{}{} // 获取信号量
 
-	// 发送HTTP GET请求到指定URL
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("%s 下载失败：%v\n", fileName, err)
-		<-sem // 释放信号量
-		return
-	}
-	defer resp.Body.Close()
+    // 发送HTTP GET请求到指定URL
+    resp, err := http.Get(url)
+    if err != nil {
+        fmt.Printf("%s 下载失败：%v\n", fileName, err)
+        <-sem // 释放信号量
+        return
+    }
+    defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("%s 下载失败，状态码：%d\n", fileName, resp.StatusCode)
-		<-sem // 释放信号量
-		return
-	}
+    if resp.StatusCode != http.StatusOK {
+        fmt.Printf("%s 下载失败，状态码：%d\n", fileName, resp.StatusCode)
+        <-sem // 释放信号量
+        return
+    }
 
-	// 从URL中提取文件扩展名
-	fileExtension := path.Ext(url)
-	fileNameWithExtension := fileName + fileExtension
+    // 从URL中提取文件扩展名
+    fileExtension := path.Ext(url)
+    fileNameWithExtension := fileName + fileExtension
 
-	// 创建目标文件并保存下载的内容
-	file, err := os.Create(fileNameWithExtension)
-	if err != nil {
-		fmt.Printf("%s 创建文件失败：%v\n", fileNameWithExtension, err)
-		<-sem // 释放信号量
-		return
-	}
-	defer file.Close()
+    // 创建目标文件并保存下载的内容
+    file, err := os.Create(fileNameWithExtension)
+    if err != nil {
+        fmt.Printf("%s 创建文件失败：%v\n", fileNameWithExtension, err)
+        <-sem // 释放信号量
+        return
+    }
+    defer file.Close()
 
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		fmt.Printf("%s 保存文件失败：%v\n", fileNameWithExtension, err)
-	}
+    _, err = io.Copy(file, resp.Body)
+    if err != nil {
+        fmt.Printf("%s 保存文件失败：%v\n", fileNameWithExtension, err)
+    }
 
-	<-sem // 释放信号量
-	fmt.Printf("%s 下载完成\n", fileNameWithExtension)
+    <-sem // 释放信号量
+    fmt.Printf("%s 下载完成\n", fileNameWithExtension)
 }
 
 func processExcelFile(filename string) {
-	// 打开Excel文件
-	excelFile, err := excelize.OpenFile(filename)
-	if err != nil {
-		fmt.Printf("打开Excel文件失败：%v\n", err)
-		return
-	}
+    excelFile, err := excelize.OpenFile(filename)
+    if err != nil {
+        fmt.Printf("打开Excel文件失败：%v\n", err)
+        dlgs.Error("错误", "打开Excel文件失败："+err.Error())
+        return
+    }
 
-	// 获取第一个工作表
-	sheetName := "Sheet1" // 指定工作表名称
-	rows, err := excelFile.GetRows(sheetName)
-	if err != nil {
-		fmt.Printf("读取工作表失败：%v\n", err)
-		return
-	}
+    sheetName := "Sheet1" // 指定工作表名称
+    sheets := excelFile.GetSheetList()
 
-	var wg sync.WaitGroup
-	sem := make(chan struct{}, 4) // 创建信号量，限制并发数为 4
+    // 检查Sheet1是否存在
+    sheetExists := false
+    for _, name := range sheets {
+        if name == sheetName {
+            sheetExists = true
+            break
+        }
+    }
 
-	for rowIndex := 0; rowIndex < len(rows); rowIndex++ {
-		row := rows[rowIndex]
+    if !sheetExists {
+        fmt.Printf("未找到工作表：%s\n", sheetName)
+        dlgs.Error("错误", fmt.Sprintf("未找到工作表：%s", sheetName))
+        return
+    }
 
-		if rowIndex == 0 {
-			continue // 跳过标题行
-		}
+    rows, err := excelFile.GetRows(sheetName)
+    if err != nil {
+        fmt.Printf("读取工作表%s失败：%v\n", sheetName, err)
+        dlgs.Error("错误", "读取工作表失败："+err.Error())
+        return
+    }
+    // 检查是否有数据
+    if len(rows) == 0 {
+        fmt.Println("工作表是空的，没有数据可读取。")
+        dlgs.Error("提示", "工作表是空的，没有数据可读取。")
+        // 这里可以添加任何适当的逻辑来处理空工作表的情况
+        return
+    }
 
-		fileNameBase := row[0]
-		for colIndex, columnValue := range row[1:] {
-			fileName := fmt.Sprintf("%s_%d", fileNameBase, colIndex+1)
-			url := columnValue
+    var wg sync.WaitGroup
+    sem := make(chan struct{}, 4) // 创建信号量，限制并发数为 4
 
-			wg.Add(1)
-			go download(fileName, url, &wg, sem)
-		}
-	}
+    for rowIndex, row := range rows {
+        if rowIndex == 0 {
+            continue // 跳过标题行
+        }
 
-	wg.Wait()
+        fileNameBase := row[0]
+        for colIndex, columnValue := range row[1:] {
+            fileName := fmt.Sprintf("%s_%d", fileNameBase, colIndex+1)
+            url := columnValue
+
+            wg.Add(1)
+            go download(fileName, url, &wg, sem)
+        }
+    }
+
+    wg.Wait()
 }
 
 func main() {
-	filePath, _, err := dlgs.File("选择Excel文件", "*.xlsx", false)
-	if err != nil {
-		fmt.Println("文件选择错误:", err)
-		return
-	}
+    filePath, _, err := dlgs.File("选择Excel文件", "*.xlsx", false)
+    if err != nil {
+        fmt.Println("文件选择错误:", err)
+        dlgs.Error("文件选择错误", err.Error())
+        return
+    }
 
-	if filePath == "" {
-		fmt.Println("没有选择文件")
-		return
-	}
+    if filePath == "" {
+        fmt.Println("没有选择文件")
+        return
+    }
 
-	processExcelFile(filePath)
+    processExcelFile(filePath)
 }
